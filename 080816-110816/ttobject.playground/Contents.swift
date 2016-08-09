3a26838e0411e45bfba9e112294d4e374351dac9
lunaticaug <nayclub@gmail.com>
2016-08-09 13:36:20 +0900

    dddd
    dddddddd

---
+++ 080816-110816/ttobject.playground/Contents.swift
@@ -0,0 +1,8 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+class Factory {
+    let bpID:Int?=nil
+    let bpType:String?=nil
+}
---
+++ ThirstyTraveler/chapter.swift
@@ -0,0 +1,9 @@
+//
+//  chapter.swift
+//  
+//
+//  Created by 양문영 on 2016. 8. 9..
+//
+//
+
+import Foundation

d743b244903b631210ad6633fefaa5dc6c179b11
wonyeong91 <wonyeong91@naver.com>
2016-08-16 13:56:53 +0900

    create Factory

--- ThirstyTraveler/ThirstyTraveler/Factory.swift
+++ ThirstyTraveler/ThirstyTraveler/Factory.swift
@@ -2,8 +2,26 @@
 //  Factory.swift
 //  ThirstyTraveler
 //
-//  Created by nariyoon on 2016. 8. 15..
+//  Created by nariyoon on 2016. 8. 16..
 //  Copyright © 2016년 moonyoung. All rights reserved.
 //
 
 import Foundation
+
+
+class Factory{
+    var OfficeTime:[Int:Int]
+    var ProgramList:[String]
+    var OfficeNumber:Int
+    var OfficeURL:String
+    
+    init(name:String ){
+        
+        self.OfficeTime = [0900:1700]
+        self.ProgramList =  [""]
+        self.OfficeNumber = 01000000000
+        self.OfficeURL = "http//"
+        
+    }
+    
+}

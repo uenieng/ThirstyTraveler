95d834335d44666a5d13adb4cc369d993e42a3d5
lunaticaug <nayclub@gmail.com>
2016-08-17 13:24:38 +0900

    breweryb
    brewery

---
+++ ThirstyTraveler/ThirstyTraveler/Brewery.swift
@@ -0,0 +1,22 @@
+//
+//  Brewery.swift
+//  ThirstyTraveler
+//
+//  Created by 양문영 on 2016. 8. 17..
+//  Copyright © 2016년 moonyoung. All rights reserved.
+//
+
+import Foundation
+
+
+class Brewery {
+    
+    typealias hours = (openHour:Int,openMinute:Int,closeHour:Int,closeHour:Int)
+    //typealias yoyo
+
+    var workingHour:hours = (0,0,0,0)
+    
+    struct brewedBeer = 
+    
+    
+}

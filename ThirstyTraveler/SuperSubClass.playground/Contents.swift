f9a403306b4958021bbfa313a77fc2b725711028
gusrn0505 <cscoi017@cscoi017ui-iMac.local>
2016-08-18 14:09:04 +0900

    dd

--- ThirstyTraveler/SuperSubClass.playground/Contents.swift
+++ ThirstyTraveler/SuperSubClass.playground/Contents.swift
@@ -20,10 +20,13 @@ class BeerPlace{
     let type:String
     var ratings:Double?
     var location:(Double,Double)
+    var wishList:Bool = false
+    var visited:Bool = false
     
-    
-    init(name:String){
+    init(name:String,type:String,location:(Double,Double)){
         self.name = name
+        self.location = location
+        self.
     }
     
     convenience init(){
@@ -52,7 +55,7 @@ class DraftBeer : BeerPlace{
     var priceList:[String:Int]
     
     init(pricelist:[String:Int]){
-        self.priceList = priceList
+        self.priceList = [:]
         super.init(name:"")
         
     }
@@ -62,3 +65,58 @@ class DraftBeer : BeerPlace{
     }
 }
 
+
+
+class Factory : BeerPlace{
+    
+    var OfficeTime:[Int:Int]
+    var ProgramList:[String]
+    var OfficeNumber:Int
+    var OfficeURL:String
+    init(){
+        
+        
+        self.OfficeTime = [0900:1700]
+        self.ProgramList =  [""]
+        self.OfficeNumber = 01000000000
+        self.OfficeURL = "http//"
+        super.init(name:"", type:"", location:(0,0))
+    }
+    
+    
+    func SetFactory(OfficeTime:[Int:Int], ProgramList:[String], OfficeNumber:Int, OfficeURL:String, name:String, type:String, location:(Double, Double))
+    {
+        self.OfficeTime = OfficeTime
+        self.ProgramList = ProgramList
+        self.OfficeNumber = OfficeNumber
+        self.OfficeURL = OfficeURL
+        self.name = name
+        self.type = type
+        self.location = location
+    }
+
+}
+
+
+
+
+class Brewery : BeerPlace{
+    
+    typealias hours = (openHour:Int,openMinute:Int,closeHour:Int,closeHour:Int)
+    typealias Product = [String:Int]
+    
+    var workingHour:hours = (0,0,0,0)
+    var brewedBeer:Array<Product> = []
+    var bottledBeer:Array<Product> = []
+
+}
+
+
+
+
+
+
+
+class Draft : BeerPlace{
+    
+}

e4634b46f5d40057dbebeb35723709e32c5078dc
gusrn0505 <cscoi017@cscoi017ui-iMac.local>
2016-08-19 11:10:49 +0900

    Draft Objects Addition
    Adding 5 Draft Objects(Dummy Data)

--- ThirstyTraveler/ThirstyTraveler/Draft.swift
+++ ThirstyTraveler/ThirstyTraveler/Draft.swift
@@ -31,3 +31,33 @@ class DraftBeer : BeerPlace{
     }
 }
 
+ //   let draft1 = DraftBeer(name:"버드나무",type:"Draft",location:(123,456),officeTime:[("월~목",1500,2400),("금,토",1700,0100),("일",0000,0000)],officeNumber : 025478546,draftPriceList:["IPA":7000,"Duh":8000,"Whaeva":7500],bottledPriceList:["버드와이저":3500,"Cass":3300,"Hite":4000])
+
+    
+    
+    var draftArray:[DraftBeer] = []
+    
+    
+    func initializeDraft() -> [DraftBeer]{
+        
+        let draft1 = DraftBeer(officeTime: [("월~목",1500,2400),("금,토",1700,0100),("일",0000,0000)],draftPriceList:["IPA":7000,"Duh":8000,"Whaeva":7500],bottledPriceList:["버드와이저":3500,"Cass":3300,"Hite":4000], OfficeNumber : 025478546, name:"버드나무",type:"Draft", location:(123,456))
+        
+        let draft2 = DraftBeer(officeTime: [("월~금",1800,2400),("토,일",1930,0100)],draftPriceList:["IPA4":7000,"Idk":8000,"Wtf":7500],bottledPriceList:["버드와이저":3500,"Cass":3300,"Hite":4000], OfficeNumber : 025478546, name:"버드나무",type:"Draft", location:(4545,456))
+        
+        let draft3 = DraftBeer(officeTime: [("월",0000,0000),("화~일",1700,2300)],draftPriceList:["IPA":7000,"Duh":8000,"Whaeva":7500],bottledPriceList:["버드와이저":3500,"Cass":3300,"Hite":4000], OfficeNumber : 025478546, name:"버드나무",type:"Draft", location:(123,456))
+        
+        let draft4 = DraftBeer(officeTime: [("월~일",1700,2400)],draftPriceList:["IPA":7000,"Duh":8000,"Whaeva":7500],bottledPriceList:["버드와이저":3500,"Cass":3300,"Hite":4000], OfficeNumber : 025478546, name:"버드나무",type:"Draft", location:(123,456))
+        
+        let draft5 = DraftBeer(officeTime: [("월~금",1800,2330),("토",1700,0230),("일",0000,0000)],draftPriceList:["IPA":7000,"Duh":8000,"Whaeva":7500],bottledPriceList:["버드와이저":3500,"Cass":3300,"Hite":4000], OfficeNumber : 025478546, name:"버드나무",type:"Draft", location:(123,456))
+        
+        
+        
+        
+        draftArray += [draft1,draft2,draft3,draft4,draft5]
+        
+        return   draftArray}
+
+
+
+
+

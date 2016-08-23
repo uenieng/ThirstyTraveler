07bd7f630f409e186c3d35d813261b8a439ff259
jeronimoxcx <cscoi017@cscoi017ui-iMac.local>
2016-08-23 14:35:40 +0900

    boodlboodl
    before map commit..

---
+++ ThirstyTraveler/ThirstyTraveler/DraftDetailTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  DraftDetailTableViewCell.swift
+//  ThirstyTraveler
+//
+//  Created by cscoi017 on 2016. 8. 23..
+//  Copyright © 2016년 moonyoung. All rights reserved.
+//
+
+import UIKit
+
+class DraftDetailTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}

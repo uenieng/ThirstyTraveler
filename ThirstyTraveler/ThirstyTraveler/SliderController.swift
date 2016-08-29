c2365ecea5c75a1c8090e421c45f4fc341f4f2f8
wonyeong91 <wonyeong91@naver.com>
2016-08-29 10:35:09 +0900

    make slider

--- ThirstyTraveler/ThirstyTraveler/SliderController.swift
+++ ThirstyTraveler/ThirstyTraveler/SliderController.swift
@@ -9,22 +9,45 @@
 import UIKit
 import GoogleMaps
 var currentValue:Double = 0.0
+var draftValue:Double = 0.0
+var breweryValue:Double = 0.0
+var factoryValue:Double = 0.0
 class SliderController: UIViewController {
     
     let viewChange = MapViewController()
     var silderView: UIView!
     
     @IBOutlet weak var Slider: UISlider!
+    @IBOutlet weak var draftSlider: UISlider!
     
+    @IBOutlet weak var brewerySlider: UISlider!
+    @IBOutlet var factorySlider: UIView!
     @IBOutlet weak var Label: UILabel!
+    @IBOutlet weak var draftLabel: UILabel!
+    @IBOutlet weak var breweryLabel: UILabel!
+    @IBOutlet weak var factoryLabel: UILabel!
     
     @IBAction func sliderValueChanged(sender: UISlider) {
         currentValue = round(Double(sender.value)*10)/10
         Label.text = "\(currentValue)"
-        
-        //viewChange.viewDidLoad()
-        
     }
+    
+    @IBAction func draftValueChanged(sender: UISlider) {
+        draftValue = round(Double(sender.value)*10)/10
+        draftLabel.text = "\(draftValue)"
+    }
+    
+    @IBAction func breweryValueChanged(sender: UISlider) {
+        breweryValue = round(Double(sender.value)*10)/10
+        breweryLabel.text = "\(breweryValue)"
+    }
+    
+    @IBAction func factoryValueChanged(sender: UISlider) {
+        factoryValue = round(Double(sender.value)*10)/10
+        factoryLabel.text = "\(factoryValue)"
+    }
+    
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
